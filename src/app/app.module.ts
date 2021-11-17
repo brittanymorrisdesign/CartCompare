b64fb6101b136e8c5cf807e2760317bb59b49473
Brittany Morris <44029053+brittanymorrisdesign@users.noreply.github.com>
2021-11-17 00:35:01 -0500

    Firebase (#2)
    * added firebase
    
    * added firebase, working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
